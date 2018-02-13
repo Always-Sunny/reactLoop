08d7f412dd9f26c4a5d80d7c98b743a336c0c7ff
Ryan <ryan.chacon103@yahoo.com>
2018-02-13 14:37:56 -0800

    working 3/13/18 2:37pm

--- client_src/src/components/Home.js
+++ client_src/src/components/Home.js
@@ -8,31 +8,28 @@ class Home extends Component {
       <div class="container-fluid">
         <div class="row no-gutters">
           <div class="col-12 col-md-4">
-            <div class="img img-1">1</div>
+            <Link to="/design"><div class="img img-1"><span>Design</span></div></Link>
           </div>
           <div class="col-md-8">
             <div class="row no-gutters">
               <div class="col-md-6">
-                <div class="img img-2">2</div>
+                <Link to="/gallery"><div class="img img-2">2</div></Link>
               </div>
               <div class="col-md-6">
-                <div class="img img-3">3</div>
+                <Link to="/education"><div class="img img-3">3</div></Link>
               </div>
             </div>
             <div class="row no-gutters">
               <div class="col-md-6">
-                <div class="img img-4">4</div>
+                <Link to="/blog"><div class="img img-4">4</div></Link>
               </div>
               <div class="col-md-6">
-                <div class="img img-5">5</div>
+                <Link to="/about"><div class="img img-5">5</div></Link>
               </div>
             </div>
           </div>
         </div>
-
       </div>
-
-
     )
   }
 }
