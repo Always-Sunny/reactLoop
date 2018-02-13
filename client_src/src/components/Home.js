7aa362a7405f7dbf9b2b0302fc86d049f34522f0
Ryan <ryan.chacon103@yahoo.com>
2018-02-12 21:38:36 -0800

    row-stacking broken

--- client_src/src/components/Home.js
+++ client_src/src/components/Home.js
@@ -6,22 +6,22 @@ class Home extends Component {
   render() {
     return (
       <div class="container-fluid">
-  <div class="row align-items-start">
-    <div class="col-12 col-md-4">
+  <div class="row row-top align-items-start">
+    <div class="col-12 col-md-4 col-img col-img-1">
       One of three columns
     </div>
-    <div class="col-12 col-md-4">
+    <div class="col-12 col-md-4 col-img col-img-2">
       One of three columns
     </div>
-    <div class="col-12 col-md-4">
+    <div class="col-12 col-md-4 col-img col-img-3">
       One of three columns
     </div>
   </div>
-  <div class="row align-items-center justify-content-end">
-    <div class="col-12 col-md-4">
+  <div class="row row-bot align-items-center justify-content-end">
+    <div class="col-12 col-md-4 col-img col-img-4">
       One of three columns
     </div>
-    <div class="col-12 col-md-4">
+    <div class="col-12 col-md-4 col-img col-img-5">
       One of three columns
     </div>
   </div>
