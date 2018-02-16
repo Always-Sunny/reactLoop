13129883eb13fac48e32382bb2bf79dcb03cad05
Ryan <ryan.chacon103@yahoo.com>
2018-02-15 22:05:04 -0800

    switching branches

--- client_src/src/components/Header.js
+++ client_src/src/components/Header.js
@@ -1,24 +1,49 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import "../assets/styles/Header.css";
 
 class Header extends Component {
   render() {
     return (
 
-      <nav className="navbar navbar-expand-lg navbar-light" style={navbarStyle}>
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
-            <Link className="nav-item nav-link" to="/">Features</Link>
-            <Link className="nav-item nav-link" to="/">Pricing</Link>
-            <Link className="nav-item nav-link disabled" to="/">Disabled</Link>
-          </div>
+      <nav className="navbar navbar-fixed-top navbar-expand-md navbar-light">
+        <div className="d-flex w-100"></div>
+        <div className="navbar-collapse collapse justify-content-center" id="collapsingNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-1">1</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-2">2</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-3">3</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-4">4</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-5">5</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-6">6</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-7">7</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-8">8</div></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"><div className="nav-img nav-img-9">9</div></Link>
+            </li>
+          </ul>
         </div>
+        <span className="navbar-text small text-truncate mt-1 w-100 text-right">always show</span>
       </nav>
+
+
+
 
     )
   }
