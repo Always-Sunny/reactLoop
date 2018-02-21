bd41ad9a5a6a58630503d1bab9c103f0b52d5183
Ryan <ryan.chacon103@yahoo.com>
2018-02-20 19:57:08 -0800

    bootstrapped footer & navbar-light

--- client_src/src/components/Home.js
+++ client_src/src/components/Home.js
@@ -5,28 +5,33 @@ import '../assets/styles/Home.css';
 class Home extends Component {
   render() {
     return (
-      <div class="container-fluid">
-        <div class="row no-gutters">
-          <div class="col-12 col-md-4">
-            <Link to="/design"><div class="img img-1"><span>Design</span></div></Link>
+      <div className="container-fluid">
+        <div className="row no-gutters">
+          <div className="col-12 col-md-4">
+            <Link to="/design"><div className="img img-1"><span>Design</span></div></Link>
           </div>
-          <div class="col-md-8">
-            <div class="row no-gutters">
-              <div class="col-md-6">
-                <Link to="/gallery"><div class="img img-2">2</div></Link>
+          <div className="col-md-8">
+            <div className="row no-gutters">
+              <div className="col-md-6">
+                <Link to="/gallery"><div className="img img-2">2</div></Link>
               </div>
-              <div class="col-md-6">
-                <Link to="/education"><div class="img img-3">3</div></Link>
-              </div>
-            </div>
-            <div class="row no-gutters">
-              <div class="col-md-6">
-                <Link to="/blog"><div class="img img-4">4</div></Link>
-              </div>
-              <div class="col-md-6">
-                <Link to="/about"><div class="img img-5">5</div></Link>
+              <div className="col-md-6">
+                <Link to="/education"><div className="img img-3">3</div></Link>
               </div>
             </div>
+            <div className="row no-gutters">
+              <div className="col-md-6">
+                <Link to="/blog"><div className="img img-4">4</div></Link>
+              </div>
+              <div className="col-md-6">
+                <Link to="/about"><div className="img img-5">5</div></Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row no-gutters">
+          <div className="col-12 d-flex justify-content-center">
+            <div className="footer-text">Hello, please feel free to contact me via any of the social media links above.</div>
           </div>
         </div>
       </div>
