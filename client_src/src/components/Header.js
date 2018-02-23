2e3de7834712e62717ddddf811e5a73f9529c52b
Ryan <ryan.chacon103@yahoo.com>
2018-02-22 21:15:38 -0800

    added actual social media links

--- client_src/src/components/Header.js
+++ client_src/src/components/Header.js
@@ -10,24 +10,24 @@ class Header extends Component {
         <div className="container-fluid">
           <ul className="nav navbar-nav">
             <li className="nav-item nav-home nav-img">
-              <Link to="/" className="nav-link link-home"></Link>
+              <a href="/" className="nav-link link-home"></a>
             </li>
           </ul>
           <ul className="nav navbar-nav mx-auto nav-ul">
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link etsy-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link sc-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link medium-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link spotify-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link twitter-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link skype-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link ln-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link fb-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link github-link img-link"></Link></li>
-            <li className="nav-item nav-sml"><Link to="/" className="nav-link gmail-link img-link"></Link></li>
+            <li className="nav-item nav-sml"><a href="https://www.etsy.com/shop/AlwaysSunnySD/" target="_blank" className="nav-link etsy-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://soundcloud.com/alwayssunnysd" className="nav-link sc-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://medium.com/@always_sunny" target="_blank" className="nav-link medium-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://open.spotify.com/user/1244883989?si=z-k7wiw4QJ6aZ6QtZM8rzA" target="_blank" className="nav-link spotify-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://twitter.com/OhRyanC" target="_blank" className="nav-link twitter-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="skype:ryan.chacon103?chat" className="nav-link skype-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://www.linkedin.com/in/ryan-chacon/" target="_blank" className="nav-link ln-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://www.facebook.com/chacon.ryan" target="_blank" className="nav-link fb-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="https://github.com/always-sunny" target="_blank" className="nav-link github-link img-link"></a></li>
+            <li className="nav-item nav-sml"><a href="mailto:ryanandrewchacon@gmail.com" className="nav-link gmail-link img-link"></a></li>
           </ul>
           <ul className="nav navbar-nav nav-prof nav-img">
             <li className="nav-item">
-              <Link to="/" className="nav-link"></Link>
+              <a href="/" className="nav-link"></a>
             </li>
           </ul>
         </div>
